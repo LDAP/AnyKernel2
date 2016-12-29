51cc719e853df9bf3d622bc7024ad186796780bf
LDAP <lucasd.alber@gmail.com>
2016-12-29 10:29:57 +0100

    Initail CM+

--- anykernel.sh
+++ anykernel.sh
@@ -3,19 +3,21 @@
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=DirtyV by bsmitty83 @ xda-developers
-do.devicecheck=1
-do.initd=1
+kernel.string=CM+ Kernel by LDAP98 @ xda-developers
+do.devicecheck=0
+do.initd=0
 do.modules=0
 do.cleanup=1
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+device.name1=titan
+device.name2=
+device.name3=
 device.name4=
 device.name5=
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/platform/msm_sdcc.1/by-name/boot;
+initd=/system/etc/init.d;
+bindir=/system/bin;
 is_slot_device=0;
 
 
@@ -27,40 +29,45 @@ is_slot_device=0;
 ## AnyKernel permissions
 # set permissions for included ramdisk files
 chmod -R 755 $ramdisk
-chmod 644 $ramdisk/sbin/media_profiles.xml
+chmod 644 $ramdisk/sbin/post_boot_script.sh
 
 
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
+ui_print " "; ui_print "Tweaking ramdisk...";
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-append_file init.rc "run-parts" init;
+# init.qcom.rc
+backup_file init.qcom.rc;
+replace_line init.qcom.rc "    start mpdecision" "    stop mpdecision";
+append_file init.qcom.rc "post_boot_script" init.qcom.patch;
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "dvbootscript" init.tuna;
 
-# init.superuser.rc
-if [ -f init.superuser.rc ]; then
-  backup_file init.superuser.rc;
-  replace_string init.superuser.rc "Superuser su_daemon" "# su daemon" "\n# Superuser su_daemon";
-  prepend_file init.superuser.rc "SuperSU daemonsu" init.superuser;
-else
-  replace_file init.superuser.rc 750 init.superuser.rc;
-  insert_line init.rc "init.superuser.rc" after "on post-fs-data" "    import /init.superuser.rc";
-fi;
+# add frandom compatibility
+backup_file ueventd.rc;
+insert_line ueventd.rc "frandom" after "urandom" "/dev/frandom              0666   root       root\n";
+insert_line ueventd.rc "erandom" after "urandom" "/dev/erandom              0666   root       root\n";
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "nodiratime,barrier=0" "nodev,noatime,nodiratime,barrier=0,data=writeback,noauto_da_alloc,discard";
-patch_fstab fstab.tuna /cache ext4 options "barrier=0,nomblk_io_submit" "nosuid,nodev,noatime,nodiratime,errors=panic,barrier=0,nomblk_io_submit,data=writeback,noauto_da_alloc";
-patch_fstab fstab.tuna /data ext4 options "nomblk_io_submit,data=writeback" "nosuid,nodev,noatime,errors=panic,nomblk_io_submit,data=writeback,noauto_da_alloc";
-append_file fstab.tuna "usbdisk" fstab;
+backup_file file_contexts;
+insert_line file_contexts "frandom" after "urandom" "/dev/frandom				u:object_r:frandom_device:s0\n";
+insert_line file_contexts "erandom" after "urandom" "/dev/erandom				u:object_r:erandom_device:s0\n";
+
+# xPrivacy
+# Thanks to @Shadowghoster & @@laufersteppenwolf
+param=$(grep "xprivacy" service_contexts)
+if [ -z $param ]; then
+    echo -ne "xprivacy453                               u:object_r:system_server_service:s0\n" >> service_contexts
+fi
+
+# Selinux
+mount -o remount,ro /system;
+
+cmdtmp=`cat $split_img/*-cmdline`;
+case "$cmdtmp" in
+     *selinux=permissive*) ui_print " "; ui_print "SElinux Its Already Permissive..."; ;;
+     *) ui_print " "; ui_print "Setting SElinux To Permissive..."; echo "$cmdtmp androidboot.selinux=permissive" > $split_img/*-cmdline ;;
+esac;
 
 # end ramdisk changes
 
---
+++ ramdisk/sbin/post_boot_script.sh
@@ -0,0 +1,39 @@
+#!/system/bin/sh
+
+############################
+echo "Boot Script Started" | tee /dev/kmsg
+
+# Other
+echo 250 > /sys/class/kgsl/kgsl-3d0/wake_timeout
+echo "0" /dev/cpuctl/cpu.notify_on_migrate;
+
+#PowerHAL
+mv /system/vendor/lib/hw/power.msm8226.so /system/vendor/lib/hw/power.msm8226.bak
+
+# Arch_power
+echo "ARCH_POWER" > /sys/kernel/debug/sched_features
+echo "1" > /sys/kernel/sched/arch_power
+
+# Hotplugging & Govs
+stop mpdecision
+echo 1 > /sys/module/autosmp/parameters/enabled
+
+
+# Power Suspend
+echo "3" > /sys/kernel/power_suspend/power_suspend_mode
+echo "0" > /sys/kernel/power_suspend/power_suspend_state
+
+# Scheduler and Read Ahead
+echo 1 > /sys/block/mmcblk0/queue/nomerges
+echo 2 > /sys/block/mmcblk0/queue/rq_affinity
+echo 0 > /sys/block/mmcblk0/queue/add_random
+
+# Disable debugging
+echo "0" > /sys/module/binder/parameters/debug_mask
+echo "0" > /sys/module/kernel/parameters/initcall_debug
+echo "0" > /sys/module/alarm/parameters/debug_mask
+echo "0" > /sys/module/xt_qtaguid/parameters/debug_mask
+echo "0" > /sys/module/alarm_dev/parameters/debug_mask
+
+
+echo "Post-Boot Script Completed!" | tee /dev/kmsg
